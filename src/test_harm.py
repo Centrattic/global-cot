df2829f02a1b8313f86967dd638efe9383df3fdd
centrattic <pialityagi@gmail.com>
2025-10-04 21:22:27 +0000

    removing merge corrupted files

--- src/test_harm.py
+++ src/test_harm.py
@@ -1,7 +1,20 @@
 from openai_harmony import (
     load_harmony_encoding,
     HarmonyEncodingName,
+<<<<<<< Updated upstream
 )
 
 test_gpt_oss = load_harmony_encoding(HarmonyEncodingName.HARMONY_GPT_OSS)
 print(test_gpt_oss)
+=======
+    Role,
+    Message,
+    Conversation,
+    DeveloperContent,
+    SystemContent,
+    ReasoningEffort
+)
+
+test = load_harmony_encoding(HarmonyEncodingName.HARMONY_GPT_OSS)
+print(test)
+>>>>>>> Stashed changes
